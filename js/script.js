c7f5459c2244454c5c0a8cba2a5736d0a919c2d4
Daniela Klein Santos da Silva <dasilva@rafainpalace.corp>
2026-08-12 14:15:41 -0300

    registre-se

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ const titulo = "POKEDEX";
 
 document.title = titulo;
 document.getElementById("titulo-site").textContent = titulo;
+
+
+const title = "REGISTRE-SE";
+document.title = title;
+document.getElementById("login-site").textContent = title;
