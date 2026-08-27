af199fbf4a530381b48fd6e4df6f5e74b50b58d4
Daniel <danielescobarsoares@gmail.com>
2026-08-27 19:56:21 -0300

    ultima atl

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const titulo = "POKEDEX";
 document.title = titulo;
 document.getElementById("titulo-site").textContent = titulo;
 
+const subtitulo = "SEJA BEM VINDO, AO NOSSO SITE!"
 
-const title = "REGISTRE-SE";
+document.title = subtitulo;
+document.getElementById("sub-titulo").textContent = subtitulo;
+
+
+const title = "ACCOUNT";
 document.title = titulo;
 document.getElementById("login-site").textContent = title;
+
+
+const text = "LISTA DE PERSONAGENS";
+document.getElementById("text-main").textContent = text;
